ac8639f7fb3c611d497a285c23fac7ed72c0e9f1
parkerkajfasz <parkerkajfasz@gmail.com>
2024-11-12 19:05:22 -0500

    Initial commit

---
+++ src/main/java/com/parkerkajfasz/grocery_store_management/GroceryStoreManagementApplication.java
@@ -0,0 +1,13 @@
+package com.parkerkajfasz.grocery_store_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryStoreManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryStoreManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parkerkajfasz/grocery_store_management/GroceryStoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkerkajfasz.grocery_store_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryStoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
